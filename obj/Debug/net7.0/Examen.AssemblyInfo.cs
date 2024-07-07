77b65fe4b037f53e086f07dcfeb8980c3e1814cf
Mauro Clerici <Mauroclerici.7@gmail.com>
2024-07-07 10:48:12 -0300

    DB depuration

--- obj/Debug/net7.0/Examen.AssemblyInfo.cs
+++ obj/Debug/net7.0/Examen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Examen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de9d9ef5a84c41daac54ac100426e9523b1b52c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Examen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Examen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
